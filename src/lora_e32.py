f8193a86f88e5abeb80817a16a644e7afa2bbae3
Renzo Mischianti <renzo.mischianti@gmail.com>
2023-04-19 09:22:04 +0200

    Add pinout

--- src/lora_e32.py
+++ src/lora_e32.py
@@ -9,6 +9,29 @@
 #
 # This library implements the EBYTE LoRa E32 Series for RaspberryPi.
 #
+#  ____________________________________________
+# |          3V3 | 1     2 | 5V                |
+# |        GPIO2 | 3     4 | 5V                |
+# |        GPIO3 | 5     6 | GND               |
+# |        GPIO4 | 7     8 | GPIO14 (TX)       |
+# |          GND | 9    10 | GPIO15 (RX)       |
+# |       GPIO17 |11    12 | GPIO18 AUX        |
+# |       GPIO27 |13    14 | GND               |
+# |       GPIO22 |15    16 | GPIO23 M0         |
+# |          3V3 |17    18 | GPIO24 M1         |
+# |       GPIO10 |19    20 | GND               |
+# |        GPIO9 |21    22 | GPIO25            |
+# |       GPIO11 |23    24 | GPIO8             |
+# |          GND |25    26 | GPIO7             |
+# |       GPIO0  |27    28 | GPIO1             |
+# |       GPIO5  |29    30 | GND               |
+# |       GPIO6  |31    32 | GPIO12            |
+# |      GPIO13  |33    34 | GND               |
+# |      GPIO19  |35    36 | GPIO16            |
+# |          3V3 |37    38 | GPIO26            |
+# |      GPIO20  |39    40 | GND               |
+#  --------------------------------------------
+#
 # The MIT License (MIT)
 #
 # Copyright (c) 2019 Renzo Mischianti www.mischianti.org All right reserved.
